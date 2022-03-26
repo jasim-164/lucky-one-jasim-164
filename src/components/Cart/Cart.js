6c08e22ad667d7cd4dab358f733212b35729794d
jasim-164 <jasimuddincse1703164@gmail.com>
2022-03-26 08:12:03 +0600

    product name show in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,10 @@ import './Cart.css';
 const Cart = (props) => {
     const {cart}=props
     // console.log(cart);
+    //function getRandomElFromArray(arrayLenght) {
+    //    return Math.floor(Math.random() * arrayLength);
+     // }
+    
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -18,11 +22,21 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <span>Selected Items: ${cart.img} </span>
+            {cart.map((item) =>(
+                <div className='col' key={item.id}>
+                <div>
+                <img src={item.img} alt='item' height='150' width='150'></img>
+                </div>
+                <div>{item.name}</div>
+                </div>
+             )) }
+
             <p>Total price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: {tax}</p>
+            
             <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
+            <button className='btn'>Choose One</button>
         </div>
     );
 };
