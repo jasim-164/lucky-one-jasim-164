9b4c2946f8fcccf29a6f1b79221b828f97db898c
jasim-164 <jasimuddincse1703164@gmail.com>
2022-03-26 07:34:29 +0600

    cart added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './Product.css'
-const Product = ({product}) => {
-    // const {product, handleAddToCart} = props;
-    const { name, img, seller, price, ratings } = product;
+const Product = ({product,handleAddToCart}) => {
+    const { name, img, price, ratings } = product;
     
     return (
         <div className='product'>
@@ -12,8 +11,9 @@ const Product = ({product}) => {
                 <p>Price: ${price}</p>
                 <p><small>Ratings: {ratings} stars</small></p>
             </div>
-            <button  className='btn-cart'>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
                 <p className='btn-text'>Add to Cart</p>
+
             </button>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product'
+import Cart from '../Cart/Cart';
 import './Shop.css'
 
 const Shop = () => {
     const [products,setProducts]=useState([]);
+    const [cart, setCart] = useState([]);
    
     useEffect(()=>{
         fetch('products.json')
         .then(response => response.json())
         .then(data=>setProducts(data))
     },[])
+    const handleAddToCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'>
@@ -17,12 +35,14 @@ const Shop = () => {
                 {
                     products.map(product=><Product 
                         key={product.id}
+                        handleAddToCart={handleAddToCart}
                         product={product}
                         ></Product>)
                 }
             </div>
+    
             <div className="cart-container">
-                
+            <Cart cart={cart}></Cart>
             </div>
         </div>
     );
